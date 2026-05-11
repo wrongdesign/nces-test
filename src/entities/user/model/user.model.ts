07b8aed5219643bff939f11976fcb4c1485c2457
Artyom Nazyuta <resthave375@gmail.com>
2026-05-12 01:21:54 +0300

    feat: user model created

---
+++ src/entities/user/model/user.model.ts
@@ -0,0 +1,30 @@
+export enum UserStatusesEnum {
+    ACTIVE = 'active',
+    BLOCK = 'block',
+}
+
+export type UserStatuses = `${UserStatusesEnum}`;
+
+export interface UserBase {
+    index?: number;
+    id?: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    status: UserStatuses;
+}
+
+export interface UserWithPassword extends UserBase {
+    password?: string;
+    confirmPassword?: string;
+}
+
+export interface User {
+    user: UserBase;
+    access_token: string;
+}
+
+export interface OtpBody {
+    pin: string;
+}
