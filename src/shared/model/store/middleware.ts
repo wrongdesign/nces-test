bbcef30edd3d37c8b175dbed67507e1e8293ca9c
Artyom Nazyuta <resthave375@gmail.com>
2026-05-11 21:17:32 +0300

    feat: create middleware and redirect for redux store

---
+++ src/shared/model/store/middleware.ts
@@ -0,0 +1,17 @@
+import { isRejectedWithValue, type Middleware } from '@reduxjs/toolkit';
+import {redirectToLogin} from "./redirect";
+
+
+export const createRedirectionMiddleware = (): Middleware => {
+    return (store) => (next) => (action) => {
+        if (isRejectedWithValue(action)) {
+            const status = (action.payload as { status?: number })?.status;
+
+            if (status === 401 || status === 403) {
+                store.dispatch(redirectToLogin('/'));
+            }
+        }
+
+        return next(action);
+    };
+};
