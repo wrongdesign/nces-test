1b750a7bd0910fde288df2ad68bf06a83b7d2135
Artyom Nazyuta <resthave375@gmail.com>
2026-05-11 21:41:55 +0300

    feat: next-theme provider implement

--- src/features/providers/ui/Providers.tsx
+++ src/features/providers/ui/Providers.tsx
@@ -1,12 +1,23 @@
 "use client"
 
-import type {PropsWithChildren} from "react";
+import {type PropsWithChildren, Suspense} from "react";
 import ReduxProvider from "./ReduxProvider";
+import LoaderComponent from "@/shared/ui/LoaderComponent";
+import {ThemeProvider} from "next-themes";
 
 const Providers = ({ children }: PropsWithChildren) => {
     return(
         <ReduxProvider>
-            {children}
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <Suspense fallback={<LoaderComponent />}>
+                    {children}
+                </Suspense>
+            </ThemeProvider>
         </ReduxProvider>
     );
 }
