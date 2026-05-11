d8eeab73be97fcb07da403433ff53dd16699f8fe
Artyom Nazyuta <resthave375@gmail.com>
2026-05-11 23:28:55 +0300

    feat: login form created

---
+++ src/features/auth/ui/LoginForm.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import {useForm} from "react-hook-form";
+import {AuthFormType, authSchema} from "@/features/auth";
+import {zodResolver} from "@hookform/resolvers/zod";
+import DefaultFormWrapper from "@/widgets/ui/DefaultFormWrapper";
+import FormField from "@/shared/ui/FormField";
+import useLogin from "@/features/auth/api/hooks/useLogin";
+
+const LoginForm = () => {
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AuthFormType>({
+        resolver: zodResolver(authSchema),
+    });
+
+    const { loading, onSubmit } = useLogin();
+
+    return(
+        <DefaultFormWrapper
+            mainWrapperStyles={"flex flex-col gap-2!"}
+            buttonText={'Login'}
+            buttonDisabled={loading}
+            buttonSubmit={handleSubmit(onSubmit)}>
+            <FormField
+                control={control}
+                label="Email"
+                name="email"
+                errors={errors}
+                inputProps={{
+                    autoComplete: 'email',
+                    placeholder: 'email@email.com',
+                }}
+            />
+
+            <FormField
+                control={control}
+                label="Password"
+                name="password"
+                errors={errors}
+
+            />
+        </DefaultFormWrapper>
+    );
+}
+
+export default LoginForm;
