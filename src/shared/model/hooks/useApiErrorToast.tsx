1657c53aa589d2acfd88a4e9dc11fc2dd94fb072
Artyom Nazyuta <resthave375@gmail.com>
2026-05-12 00:10:06 +0300

    feat: api error toast created

---
+++ src/shared/model/hooks/useApiErrorToast.tsx
@@ -0,0 +1,34 @@
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { useEffect } from "react";
+import { SerializedError } from "@reduxjs/toolkit";
+import {toast} from "sonner";
+
+interface HandledError {
+    status: string;
+    message: string;
+}
+
+type ApiError =
+    | HandledError
+    | FetchBaseQueryError
+    | SerializedError
+    | undefined;
+
+export const useApiErrorToast = (error: ApiError | HandledError) => {
+    useEffect(() => {
+        if (error) {
+            let message = "Unknown error";
+
+            if ("message" in error && error.message) {
+                message = error.message;
+            }
+
+            toast.error(message, {
+                id: "api-error-toast",
+                position: "bottom-center",
+                className: "toast_display",
+                duration: 7000,
+            });
+        }
+    }, [error]);
+}
