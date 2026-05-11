ef39cc92f5014fa0a4d6af8ffbdb39bc076d8a7e
Artyom Nazyuta <resthave375@gmail.com>
2026-05-12 01:22:27 +0300

    feat: otp backend

---
+++ src/app/api/auth/otp/route.ts
@@ -0,0 +1,51 @@
+import {NextResponse} from 'next/server';
+import {otp, user} from "./mocks";
+import {OtpBody} from "@/entities/user";
+
+export async function POST(request: Request) {
+    try {
+        const body: OtpBody =
+            await request.json()
+
+        const { pin } = body
+
+        const otpValid = otp.includes(pin);
+
+        await new Promise((resolve) => {
+            setTimeout(resolve, 1000)
+        })
+
+        if (!otpValid) {
+            return NextResponse.json(
+                {
+                    message: "Invalid OTP",
+                },
+                {
+                    status: 401,
+                }
+            )
+        }
+
+        return NextResponse.json(
+            {
+                ...user
+            },
+            {
+                status: 200,
+            }
+        )
+    } catch (error) {
+        console.error("[AUTH_OTP]", error)
+
+        return NextResponse.json(
+            {
+                message:
+                    (error as Error).message ??
+                    "Internal Error",
+            },
+            {
+                status: 500,
+            }
+        )
+    }
+}
