e3d61f7460221af317775d1446a848b67fca0446
Artyom Nazyuta <resthave375@gmail.com>
2026-05-12 00:09:14 +0300

    feat: base fetcher and error handling

---
+++ src/shared/api/fetcher.tsx
@@ -0,0 +1,97 @@
+import type {
+    BaseQueryFn,
+    FetchArgs,
+    FetchBaseQueryMeta,
+} from "@reduxjs/toolkit/query";
+
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import {RootState} from "@/shared/model/store";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+        // const token = (getState() as RootState).auth.token;
+
+        // if (token) {
+        //     headers.set("Authorization", `Bearer ${token}`);
+        // }
+
+        headers.set("Content-Type", "application/json");
+
+        return headers;
+    },
+    credentials: "include",
+});
+
+const baseQueryWithErrorHandling: BaseQueryFn<
+    string | FetchArgs,
+    unknown,
+    {
+        status: number | string;
+        message: string;
+    },
+    unknown,
+    FetchBaseQueryMeta
+> = async (args, api) => {
+
+    const result = await baseQuery(args, api, {});
+
+    if (result.data === "OK") {
+        return { data: { success: true } };
+    }
+
+    if (result?.meta?.response?.status === 204) {
+        return { data: {} };
+    }
+
+    if (result.error) {
+        const status = result.error.status;
+        const data = result.error.data;
+
+        if (data === "OK") {
+            return { data: { success: true } };
+        }
+
+        let message = "Unknown error";
+
+        if (data && typeof data === "object") {
+            message =
+                (data as any).message ||
+                (data as any).devMessage ||
+                JSON.stringify(data);
+        } else if (typeof data === "string") {
+            message = data;
+        }
+
+        if (status === 401 || status === 403) {
+        //     TODO: Place here redirection
+        }
+
+        return {
+            error: {
+                status,
+                message,
+            },
+        };
+    }
+
+    return result;
+};
+
+export const baseFileErrorResponseHandler = (response: any) => {
+    let message = "Unknown error";
+
+    if (response.data && typeof response.data === "object") {
+        message =
+            response.data.message || response.data.devMessage || "Failed to get file";
+    } else if (typeof response.data === "string") {
+        message = response.data;
+    }
+
+    return {
+        status: response.status,
+        message: message,
+    };
+};
+
+export default baseQueryWithErrorHandling;
