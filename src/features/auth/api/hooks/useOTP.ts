4acf01c82a2213f62246cb925b202da34ac8def3
Artyom Nazyuta <resthave375@gmail.com>
2026-05-12 01:23:17 +0300

    feat: otp api hook

---
+++ src/features/auth/api/hooks/useOTP.ts
@@ -0,0 +1,40 @@
+import {useVerify2faMutation} from "@/shared/model/store/api/auth.api";
+import {useApiErrorToast} from "@/shared/model/hooks/useApiErrorToast";
+import {useRouter} from "next/dist/client/components/navigation";
+import {useTransition} from "react";
+import {useAppDispatch} from "@/shared/model/store";
+import {updateUser} from "@/shared/model/store/slices/auth/auth.slice";
+
+const useOTP = () => {
+    const dispatch = useAppDispatch();
+
+    const [sendOtp, {isLoading: sendOtpLoading, error: sendOtpError}] = useVerify2faMutation();
+
+    useApiErrorToast(sendOtpError);
+
+    const router = useRouter();
+    const [isPending, startTransition] = useTransition();
+
+    const onSubmit = async (code: string) => {
+        try {
+            const response = await sendOtp({ pin: code }).unwrap();
+
+            if (response) {
+                dispatch(updateUser(response));
+
+                startTransition(() => {
+                    router.push("/dashboard");
+                })
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return {
+        loading: sendOtpLoading || isPending,
+        onSubmit
+    };
+}
+
+export default useOTP;
