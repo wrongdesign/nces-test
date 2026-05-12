64435c9c49d94ef1e7e49c0100ccb3713c0d0378
Artyom Nazyuta <resthave375@gmail.com>
2026-05-13 01:41:27 +0300

    feat: new shadcn components

--- src/features/providers/ui/Providers.tsx
+++ src/features/providers/ui/Providers.tsx
@@ -5,6 +5,7 @@ import ReduxProvider from "./ReduxProvider";
 import LoaderComponent from "@/shared/ui/LoaderComponent";
 import {ThemeProvider, useTheme} from "next-themes";
 import {Toaster} from "@/shared/ui/sonner";
+import {TooltipProvider} from "@/shared/ui/tooltip";
 
 const Providers = ({ children }: PropsWithChildren) => {
     const { theme } = useTheme();
@@ -16,7 +17,9 @@ const Providers = ({ children }: PropsWithChildren) => {
                 forcedTheme={theme}
             >
                 <Suspense fallback={<LoaderComponent />}>
-                    {children}
+                    <TooltipProvider>
+                        {children}
+                    </TooltipProvider>
                 </Suspense>
                 <Toaster />
             </ThemeProvider>
