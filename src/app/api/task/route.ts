d060b08fdfef509bc41412c76c0ef4f449ca3120
Artyom Nazyuta <resthave375@gmail.com>
2026-05-13 00:04:54 +0300

    feat: get tasks with pagination

---
+++ src/app/api/task/route.ts
@@ -0,0 +1,61 @@
+import {type NextRequest, NextResponse} from "next/server";
+import {INTERNAL_ERROR} from "@/app/api/config/common";
+import {mockTasks} from "@/app/api/task/mocks";
+
+export async function GET(request: NextRequest) {
+    try {
+        const searchParams = request.nextUrl.searchParams
+
+        const page = Number(searchParams.get("page")) ?? undefined
+
+        const limit = Number(searchParams.get("limit")) ?? undefined
+
+        const sortedTasks = [...mockTasks].sort(
+            (a, b) => {
+                const dateA = new Date(
+                    a.createdAt
+                        .split(".")[0]
+                        .replace(" ", "T")
+                ).getTime()
+
+                const dateB = new Date(
+                    b.createdAt
+                        .split(".")[0]
+                        .replace(" ", "T")
+                ).getTime()
+
+                return dateB - dateA
+            }
+        )
+
+        const startIndex = (page - 1) * limit
+
+        const endIndex = startIndex + limit
+
+        const paginatedTasks = page && limit ?
+            sortedTasks.slice(
+                startIndex,
+                endIndex
+            ) : sortedTasks;
+
+        return NextResponse.json({
+            tasks: paginatedTasks,
+
+            ...(page && limit && {
+                pagination: {
+                    total: sortedTasks.length,
+                    totalPages: Math.ceil(
+                        sortedTasks.length / limit
+                    ),
+                    hasNextPage:
+                        endIndex < sortedTasks.length,
+                    hasPrevPage: page > 1,
+                }
+            }),
+        })
+    } catch (error) {
+        console.error("[GET_TASK]", error)
+
+        return INTERNAL_ERROR(error)
+    }
+}
