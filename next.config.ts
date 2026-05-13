11c7cd473c7a2a78ebb4586f3ac3e378a73891f1
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 02:30:04 +0300

    build: redirection policy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/dashboard/task',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
