297c431b2b72b095713d0060f24de86e65d2f7f8
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 01:37:29 +0300

    feat: update status patch

---
+++ src/app/api/task/status/[id]/route.ts
@@ -0,0 +1,51 @@
+import type {Task, TaskStatusInterface} from "@/entities/task";
+import {readFile, writeFile} from "@/app/api/task/utils/common";
+import {type NextRequest, NextResponse} from "next/server";
+import {INTERNAL_ERROR} from "@/app/api/config/common";
+
+export async function PATCH(
+    request: NextRequest,
+    context: {
+        params: Promise<{
+            id: string
+        }>
+    }
+) {
+    try {
+        const { id } = await context.params;
+
+        const { status }: TaskStatusInterface = await request.json();
+
+        const mockTasks: Task[] = readFile("src/app/api/task/mocks/tasks.json");
+
+        const selectedTask = mockTasks.find((task) => task.id === id);
+
+        if (!selectedTask)
+            return NextResponse.json(
+                {
+                    message: "Task not found",
+                },
+                {
+                    status: 500,
+                }
+            )
+
+        const newTask: Task = { ...selectedTask, status: status};
+
+        const newTasks: Task[] = mockTasks.map((task) => {
+            return task.id === id ? newTask : task;
+        });
+
+        writeFile<Task[]>("src/app/api/task/mocks/tasks.json", newTasks);
+
+        await new Promise((resolve) => {
+            setTimeout(resolve, 1000)
+        })
+
+        return NextResponse.json(newTask)
+    } catch (error) {
+        console.error("[PATCH_STATUS]", error)
+
+        return INTERNAL_ERROR(error)
+    }
+}
