17f1a2caa02c21b9ef354dddb41eaa8f5df8b59e
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 23:42:45 +0300

    feat: upgrade get tasks for filters use

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,15 +1,17 @@
 import {type NextRequest, NextResponse} from "next/server";
 import {INTERNAL_ERROR} from "@/app/api/config/common";
 import {readFile} from "@/app/api/task/utils/common";
-import type {Task} from "@/entities/task";
+import type {PriorityType, Task, TaskStatusType} from "@/entities/task";
 
 export async function GET(request: NextRequest) {
     try {
         const searchParams = request.nextUrl.searchParams
 
         const page = Number(searchParams.get("page")) ?? undefined
-
         const limit = Number(searchParams.get("limit")) ?? undefined
+        const status: TaskStatusType | undefined = searchParams.get("status") as TaskStatusType
+        const priority: PriorityType | undefined = searchParams.get("priority") as PriorityType
+        const tag: string | undefined = searchParams.get("tag") ?? undefined
 
         const mockTasks: Task[] = readFile("src/app/api/task/mocks/tasks.json");
 
@@ -31,27 +33,35 @@ export async function GET(request: NextRequest) {
             }
         )
 
+        const filteredTasks = sortedTasks.filter(() => {
+            const filterByStatus = status ? sortedTasks.filter((task: Task) => task.status === status) : sortedTasks;
+
+            const filterByPriority = priority ? filterByStatus.filter((task: Task) => task.priority === priority) : filterByStatus;
+
+            return tag ? filterByPriority.filter((task: Task) => task.tags.includes(tag)) : filterByPriority;
+        })
+
         const startIndex = (page - 1) * limit
 
         const endIndex = startIndex + limit
 
         const paginatedTasks = page && limit ?
-            sortedTasks.slice(
+            filteredTasks.slice(
                 startIndex,
                 endIndex
-            ) : sortedTasks;
+            ) : filteredTasks;
 
         return NextResponse.json({
             tasks: paginatedTasks,
 
             ...(page && limit && {
                 pagination: {
-                    total: sortedTasks.length,
+                    total: filteredTasks.length,
                     totalPages: Math.ceil(
-                        sortedTasks.length / limit
+                        filteredTasks.length / limit
                     ),
                     hasNextPage:
-                        endIndex < sortedTasks.length,
+                        endIndex < filteredTasks.length,
                     hasPrevPage: page > 1,
                 }
             }),
