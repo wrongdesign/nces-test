8b921b4a0dd8521946fa6a9c75a9ed475a041cb9
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 03:16:41 +0300

    feat: task details created

---
+++ src/features/task/ui/details/TaskDetails.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import {Button} from "@/shared/ui/button";
+import {useAppSelector} from "@/shared/model/store";
+import {TaskMode, type TaskModeType, TaskStatusEnum} from "@/entities/task";
+import {TaskDetailsInfoForm} from "@/features/task";
+
+interface Props {
+    mainDisabler: boolean;
+    taskMode: TaskModeType;
+}
+
+const TaskDetails = ({ mainDisabler, taskMode }: Props) => {
+    const task = useAppSelector((state) => state.task.selectedTask);
+
+    const disableConfirm: boolean = false;
+    const submitButtonText: string = "Submit";
+
+    return(
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-row justify-between items-center">
+                <div className={'flex flex-row gap-4'}>
+                    {!mainDisabler && (
+                        <Button
+                            variant="secondary"
+                            disabled={disableConfirm}
+                            onClick={async () => {}}
+                        >
+                            {submitButtonText}
+                        </Button>
+                    )}
+                    {(task?.status === TaskStatusEnum.INPROGRESS || task?.status === TaskStatusEnum.DONE) && (
+                        <Button className="defaultButton" onClick={() => {}}>
+                            Close task
+                        </Button>
+                    )}
+                </div>
+            </div>
+
+            <TaskDetailsInfoForm />
+
+            {/* TODO: Place here logic for comments and history*/}
+            {/*{taskMode === TaskMode.WORKING }*/}
+        </div>
+    );
+}
+
+export default TaskDetails;
