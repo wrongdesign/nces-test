3508e9788477acbda323cf9e8c557f27805f7426
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 23:56:23 +0300

    feat: display filters

--- src/features/task/ui/TaskList.tsx
+++ src/features/task/ui/TaskList.tsx
@@ -3,7 +3,7 @@
 import useGetTasks from "@/features/task/api/hooks/useGetTasks";
 import LoaderComponent from "@/shared/ui/LoaderComponent";
 import {useAppDispatch, useAppSelector} from "@/shared/model/store";
-import {TaskComponent} from "@/features/task";
+import {FiltersBlock, TaskComponent} from "@/features/task";
 import {useEffect} from "react";
 import {setFetchTags, setFetchTasks} from "@/shared/model/store/slices/task/task.slice";
 
@@ -20,14 +20,19 @@ const TaskList = () => {
     }, [dispatch]);
 
     return(
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {tasks && tasks.length > 0 && (
-                tasks.map((task) => (
-                    <TaskComponent key={task.id} {...task} tagsList={tags} />
-                ))
-            )}
-            {loading && <LoaderComponent fixed={true} />}
+        <div className="flex flex-col gap-2">
+            <FiltersBlock tags={tags} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {tasks && tasks.length > 0 && (
+                    tasks.map((task) => (
+                        <TaskComponent key={task.id} {...task} tagsList={tags} />
+                    ))
+                )}
+                {loading && <LoaderComponent fixed={true} />}
+            </div>
         </div>
+
     );
 }
 
