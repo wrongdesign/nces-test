c1fb9e40012e977f9571744a14986c10363b26ec
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 03:38:08 +0300

    feat: nav menu item interface

---
+++ src/widgets/NavMenu/types/types.ts
@@ -0,0 +1,8 @@
+import type {iconMap} from "@/widgets/NavMenu/config/config";
+
+export interface INavMenuItem {
+    location: string;
+    icon?: keyof typeof iconMap;
+    text: string;
+    disabled?: boolean;
+}
