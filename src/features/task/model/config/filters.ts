d00d2b6e079b54731102afee1e87ba1d081fbce6
Artyom Nazyuta <resthave375@gmail.com>
2026-05-14 23:54:58 +0300

    feat: filters config

---
+++ src/features/task/model/config/filters.ts
@@ -0,0 +1,4 @@
+import {cn} from "@/shared/model/utils/utils";
+
+export const selectTriggerClassNames = cn("flex! flex-row text-left cursor-pointer w-full");
+export const selectItemClassNames = cn("cursor-pointer");
