68ff2393ba08f8395420fa50302e53a9e3f839e6
Artyom Nazyuta <resthave375@gmail.com>
2026-05-15 01:18:17 +0300

    feat: debounce hook based on generic

---
+++ src/shared/model/hooks/useDebounce.ts
@@ -0,0 +1,26 @@
+"use client"
+
+import {useEffect, useState} from "react";
+
+interface Props<T> {
+    value: T;
+    delay: number;
+}
+
+const useDebounce = <T>({ value, delay }: Props<T>) => {
+    const [debouncedVal, setDebouncedVal] = useState<T>(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedVal(value);
+        }, delay);
+
+        return () => clearTimeout(timer);
+    }, [value, delay]);
+
+    return {
+        debouncedVal,
+    }
+}
+
+export default useDebounce
