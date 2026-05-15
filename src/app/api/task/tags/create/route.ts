3018be39bf95070ef4abb94a25a50bf39bc00d21
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 02:24:00 +0300

    feat: request for tag creation

---
+++ src/app/api/task/tags/create/route.ts
@@ -0,0 +1,33 @@
+import type {Tag} from "@/entities/task";
+import {readFile, writeFile} from "@/app/api/task/utils/common";
+import {type NextRequest, NextResponse} from "next/server";
+import {INTERNAL_ERROR} from "@/app/api/config/common";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { name }: Pick<Tag, "name"> = await request.json();
+
+        const id = crypto.randomUUID();
+
+        const newTag: Tag = {
+            id: id,
+            name,
+        }
+
+        const mockTags: Tag[] = readFile("src/app/api/task/mocks/tags.json");
+
+        const newTagsList: Tag[] = [...mockTags, newTag];
+
+        writeFile("src/app/api/task/mocks/tags.json", newTagsList);
+
+        return new NextResponse(null,
+            {
+                status: 204,
+            }
+        );
+    } catch (error) {
+        console.error("[CREATE_TAG]", error)
+
+        return INTERNAL_ERROR(error)
+    }
+}
