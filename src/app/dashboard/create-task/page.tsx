7abae43ddad75c0c388785d245e4c30e218cf2d1
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 02:23:18 +0300

    feat: create task page

---
+++ src/app/dashboard/create-task/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import BackButton from "@/widgets/BackButton/BackButton";
+import {TaskDetails, useGetTags} from "@/features/task";
+import {TaskMode} from "@/entities/task";
+import DefaultPageWrapper from "@/shared/ui/DefaultPageWrapper";
+import {useAppDispatch} from "@/shared/model/store";
+import {useEffect} from "react";
+import {setFetchTags} from "@/shared/model/store/slices/task/task.slice";
+import LoaderComponent from "@/shared/ui/LoaderComponent";
+
+const CreateTaskPage = () => {
+    const dispatch = useAppDispatch();
+
+    const { tagsLoading } = useGetTags();
+
+    useEffect(() => {
+        dispatch(setFetchTags(true));
+    }, [dispatch]);
+
+    return(
+        <DefaultPageWrapper
+            backButton={<BackButton />}
+            title={"Create Task"}
+        >
+            <TaskDetails taskMode={TaskMode.CREATE} />
+
+            {tagsLoading && <LoaderComponent />}
+        </DefaultPageWrapper>
+    );
+}
+
+export default CreateTaskPage;
