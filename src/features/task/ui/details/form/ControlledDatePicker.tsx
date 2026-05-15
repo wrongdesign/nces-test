dec4ba10b61a911b8f371178ad28ca1936995d74
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 00:32:48 +0300

    feat: controlled date picker

---
+++ src/features/task/ui/details/form/ControlledDatePicker.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import {Controller, type FieldError, type FieldValues} from "react-hook-form";
+import type {CreateTaskForm} from "@/features/task";
+import {Label} from "@/shared/ui/label";
+import type React from "react";
+import {Popover, PopoverContent, PopoverTrigger} from "@/shared/ui/popover";
+import {Button} from "@/shared/ui/button";
+import {CalendarIcon} from "lucide-react";
+import {Calendar} from "@/shared/ui/calendar";
+import {formatDate} from "@/shared/model/utils/date";
+
+interface Props {
+    label: string;
+}
+
+const ControlledDatePicker = <T extends FieldValues>({ control, errors, name, label }: CreateTaskForm<T> & Props) => {
+    return(
+            <Popover>
+                <div className={"flex flex-col gap-2"}>
+                    <Label>{label}</Label>
+                    <Controller
+                        control={control}
+                        name={name}
+                        render={({ field: { onChange, onBlur, value } }) => {
+                            return (
+                                <>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant="outline"
+                                            data-empty={!value}
+                                            onBlur={onBlur}
+                                            className="justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
+                                        >
+                                            <CalendarIcon />
+                                            {value ? formatDate(value, 'sv-SE', 'string') : <span>Pick date</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0">
+                                        <Calendar mode="single" selected={value} onSelect={(value) => onChange(value?.toLocaleDateString())} />
+                                    </PopoverContent>
+                                </>
+                            )
+                        }}
+                    />
+
+                    {errors?.[name] && (
+                        <span className="text-sm text-red-500">
+                            {(errors[name] as FieldError).message}
+                        </span>
+                    )}
+                </div>
+            </Popover>
+    );
+}
+
+export default ControlledDatePicker;
