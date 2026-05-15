c8956b75b2290cbe81a81dfebc214c66dd407071
Artyom Nazyuta <resthave375@gmail.com>
2026-05-15 03:54:15 +0300

    feat: create task schema

---
+++ src/features/task/model/schemas/details.schema.ts
@@ -0,0 +1,33 @@
+import { z } from "zod"
+import {PriorityEnum, TaskStatusEnum} from "@/entities/task";
+
+export const taskStatusSchema = z.object({
+    status: z.enum(TaskStatusEnum),
+})
+
+export const taskSchema = taskStatusSchema.extend({
+    title: z
+        .string("Title is required")
+        .min(5, "Minimum 5 symbols")
+        .max(256, "Maximum 256 symbols"),
+    description: z
+        .string()
+        .max(500, "Maximum 500 symbols")
+        .optional(),
+    priority: z.enum(PriorityEnum, "Select 1 of priority"),
+    deadline: z
+        .string('Deadline is required')
+        .min(1, { message: 'Deadline is required' })
+        .refine((date) => !isNaN(Date.parse(date)), {
+            message: 'Invalid date format',
+        }),
+    tags: z
+        .array(z.string())
+        .min(1, {
+            message:
+                "At least one tag is required",
+        }),
+    status: z.enum(TaskStatusEnum, "Select 1 of status"),
+})
+
+export type TaskSchemaType = z.infer<typeof taskSchema>
