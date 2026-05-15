cd2d1fcd466b27ac56e118fd7f3645a40af8ff28
Artyom Nazyuta <resthave375@gmail.com>
2026-05-15 03:58:35 +0300

    refactor: details display change

--- src/features/task/ui/details/TaskDetails.tsx
+++ src/features/task/ui/details/TaskDetails.tsx
@@ -13,31 +13,17 @@ interface Props {
 const TaskDetails = ({ mainDisabler, taskMode }: Props) => {
     const task = useAppSelector((state) => state.task.selectedTask);
 
-    const disableConfirm: boolean = false;
-    const submitButtonText: string = "Submit";
-
     return(
-        <div className="flex flex-col gap-4">
-            <div className="flex flex-row justify-between items-center">
-                <div className={'flex flex-row gap-4'}>
-                    {!mainDisabler && (
-                        <Button
-                            variant="secondary"
-                            disabled={disableConfirm}
-                            onClick={async () => {}}
-                        >
-                            {submitButtonText}
-                        </Button>
-                    )}
-                    {(task?.status !== TaskStatusEnum.DONE) && (
-                        <Button variant="destructive" className="cursor-pointer" onClick={() => {}}>
-                            Close task
-                        </Button>
-                    )}
-                </div>
-            </div>
-
+        <div className="flex flex-col gap-4 w-full!">
             <TaskDetailsInfoForm />
+
+            <div className={'flex justify-end w-full!'}>
+                {(task?.status !== TaskStatusEnum.DONE) && (
+                    <Button variant="destructive" className="cursor-pointer" onClick={() => {}}>
+                        Close task
+                    </Button>
+                )}
+            </div>
 
             {/* TODO: Place here logic for comments and history*/}
             {/*{taskMode === TaskMode.WORKING }*/}
