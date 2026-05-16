78e6dec2a0a45f9433f286296d20eca75907abad
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 20:47:59 +0300

    feat: useGetTaskDetails api hook

---
+++ src/features/task/api/hooks/useGetTaskDetails.ts
@@ -0,0 +1,31 @@
+"use client"
+
+import {useGetTaskByIdQuery} from "@/shared/model/store/api/task.api";
+import {useAppDispatch, useAppSelector} from "@/shared/model/store";
+import {useApiErrorToast} from "@/shared/model/hooks/useApiErrorToast";
+import {useEffect} from "react";
+import {setUpdateSelectedTask} from "@/shared/model/store/slices/task/task.slice";
+
+const useGetTaskDetails = () => {
+    const dispatch = useAppDispatch()
+    const taskId = useAppSelector(state => state.task.selectedTask);
+    const taskFetch = useAppSelector(state => state.task.updateSelectedTask);
+
+    const { data: task, isLoading: getTaskLoading, error: getTaskError, refetch: taskRefetch } = useGetTaskByIdQuery(taskId ?? { id: "" });
+
+    useApiErrorToast(getTaskError);
+
+    useEffect(() => {
+        if (taskFetch) {
+            taskRefetch();
+            dispatch(setUpdateSelectedTask(false));
+        }
+    }, [taskFetch, taskRefetch, dispatch]);
+
+    return {
+        task,
+        getTaskLoading,
+    }
+}
+
+export default useGetTaskDetails;
