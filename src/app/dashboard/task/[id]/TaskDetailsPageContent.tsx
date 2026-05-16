e4047cc8279b5ebd0f22f52055965c5b1c2245e4
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 20:50:51 +0300

    refactor: valid handle params

---
+++ src/app/dashboard/task/[id]/TaskDetailsPageContent.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import {useAppDispatch, useAppSelector} from "@/shared/model/store";
+import {useEffect} from "react";
+import {setSelectedTask, setUpdateSelectedTask} from "@/shared/model/store/slices/task/task.slice";
+import DefaultPageWrapper from "@/shared/ui/DefaultPageWrapper";
+import BackButton from "@/widgets/BackButton/BackButton";
+import {cn} from "@/shared/model/utils/utils";
+import {TaskDetails} from "@/features/task";
+import {TaskMode} from "@/entities/task";
+
+const TaskDetailsPageContent = ({ id }: { id: string }) => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (id) {
+            dispatch(setSelectedTask(id));
+            dispatch(setUpdateSelectedTask(true));
+        }
+
+        return () => {
+            dispatch(setSelectedTask(undefined));
+            dispatch(setUpdateSelectedTask(false));
+        }
+    }, [id, dispatch]);
+
+    const expired = useAppSelector(state => state.task.taskExpired);
+    return(
+        <DefaultPageWrapper
+            backButton={<BackButton />}
+            title={"Task Details"}
+            additionalStyles={cn(expired && "border border-destructive!")}
+        >
+            <TaskDetails taskMode={TaskMode.WORKING} />
+        </DefaultPageWrapper>
+    );
+}
+
+export default TaskDetailsPageContent;
--- src/app/dashboard/task/[id]/page.tsx
+++ src/app/dashboard/task/[id]/page.tsx
@@ -1,22 +1,10 @@
-"use client"
+import TaskDetailsPageContent from "@/app/dashboard/task/[id]/TaskDetailsPageContent";
 
-import BackButton from "@/widgets/BackButton/BackButton";
-import {TaskDetails} from "@/features/task";
-import {TaskMode} from "@/entities/task";
-import DefaultPageWrapper from "@/shared/ui/DefaultPageWrapper";
-import {cn} from "@/shared/model/utils/utils";
-import {useAppSelector} from "@/shared/model/store";
+const TaskDetailsPage = async ({ params }: { params: Promise<{ id: string }>; }) => {
+    const { id } = await params;
 
-const TaskDetailsPage = ({ id }: {id: string}) => {
-    const expired = useAppSelector(state => state.task.taskExpired);
     return(
-        <DefaultPageWrapper
-            backButton={<BackButton />}
-            title={"Task Details"}
-            additionalStyles={cn(expired && "border border-destructive!")}
-        >
-            <TaskDetails taskMode={TaskMode.WORKING} />
-        </DefaultPageWrapper>
+        <TaskDetailsPageContent id={id} />
     );
 }
 
