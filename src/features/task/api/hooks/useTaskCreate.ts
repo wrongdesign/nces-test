26137d45e1b4c8f12aff0160a0ef432ca7934667
Artyom Nazyuta <resthave375@gmail.com>
2026-05-17 02:31:51 +0300

    fix: refine deadline for 23:59:59 time set

--- src/features/task/api/hooks/useTaskCreate.ts
+++ src/features/task/api/hooks/useTaskCreate.ts
@@ -5,6 +5,7 @@ import {useApiErrorToast} from "@/shared/model/hooks/useApiErrorToast";
 import {useTransition} from "react";
 import type {TaskSchemaType} from "@/features/task";
 import {useRouter} from "next/navigation";
+import {formatDate} from "@/shared/model/utils/date";
 
 const useTaskCreate = () => {
     const [createTask, { isLoading: createTaskLoading, error: createTaskError }] = useCreateTaskMutation();
@@ -16,11 +17,14 @@ const useTaskCreate = () => {
 
     const handleCreateTask = async (data: TaskSchemaType) => {
         try {
-            await createTask(data).unwrap();
+            await createTask({
+                ...data,
+                deadline: `${formatDate(data.deadline, "sv-SE", "string")} 23:59:59`
+            }).unwrap();
 
             startTransition(() => {
                 router.replace("/dashboard")
-            })
+            });
         } catch (e) {
             console.error(e);
         }
