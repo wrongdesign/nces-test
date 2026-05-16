2dafb1ba92ee905ce2f5facc7de009e722695a9d
Artyom Nazyuta <resthave375@gmail.com>
2026-05-16 20:48:10 +0300

    feat: useTaskDetailsActions api hook

---
+++ src/features/task/api/hooks/useTaskDetailsActions.ts
@@ -0,0 +1,22 @@
+"use client"
+
+import {useTaskCreate, useTaskUpdate, useUpdateTaskStatus} from "@/features/task";
+
+const useTaskDetailsActions = () => {
+    const { onSubmit: createTaskSubmit, loading: createTaskLoading } = useTaskCreate();
+
+    const { handleUpdateTaskStatus, updateStatusLoading } = useUpdateTaskStatus();
+
+    const { updateTaskLoading, handleUpdateTask } = useTaskUpdate()
+
+    const loading: boolean = createTaskLoading || updateStatusLoading || updateTaskLoading;
+
+    return {
+        loading,
+        handleUpdateTaskStatus,
+        createTaskSubmit,
+        handleUpdateTask,
+    }
+}
+
+export default useTaskDetailsActions;
