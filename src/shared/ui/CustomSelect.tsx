972ec9d4e3c983aa09fc9df6e303f63c18c06d8f
Artyom Nazyuta <resthave375@gmail.com>
2026-05-17 22:37:17 +0300

    refactor: minor rework

--- src/shared/ui/CustomSelect.tsx
+++ src/shared/ui/CustomSelect.tsx
@@ -3,6 +3,7 @@
 import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/shared/ui/select";
 import {selectItemClassNames, selectTriggerClassNames} from "@/features/task";
 import type {PropsWithChildren} from "react";
+import {Label} from "@/shared/ui/label";
 
 interface Props {
     value: string | undefined;
@@ -13,24 +14,27 @@ interface Props {
 
 const CustomSelect = ({ value, selectedValue, children, onValueChange, name }: PropsWithChildren<Props>) => {
     return(
-        <Select
-        value={value ?? "all"}
-        onValueChange={onValueChange}
-    >
-        <SelectTrigger className={selectTriggerClassNames}>
-            <SelectValue>
-                <p className="text-sm">{selectedValue ?? "All"}</p>
-            </SelectValue>
-        </SelectTrigger>
-        <SelectContent className={'p-0'}>
-            <SelectGroup>
-                <SelectItem value={"all"} key={`all-search-${name}-select`} className={selectItemClassNames}>
-                    All
-                </SelectItem>
-                {children}
-            </SelectGroup>
-        </SelectContent>
-    </Select>
+        <div className="flex flex-col gap-1">
+            <Label className="text-sm">{name.toUpperCase()}</Label>
+            <Select
+                value={value ?? "all"}
+                onValueChange={onValueChange}
+            >
+                <SelectTrigger className={selectTriggerClassNames}>
+                    <SelectValue>
+                        <p className="text-sm">{selectedValue ?? "All"}</p>
+                    </SelectValue>
+                </SelectTrigger>
+                <SelectContent className={'p-0'}>
+                    <SelectGroup>
+                        <SelectItem value={"all"} key={`all-search-${name}-select`} className={selectItemClassNames}>
+                            All
+                        </SelectItem>
+                        {children}
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+        </div>
     );
 }
 
